e9efbde9c72e25bc9e1014ca7c7e1c84bf641810
Maksym <112901851+Maksym82@users.noreply.github.com>
2024-07-25 11:28:20 +0300

    Create modal-rent.js

---
+++ js/modal-rent.js
@@ -0,0 +1,25 @@
+const refs = {
+    form: document.querySelector('.rent-form'),
+    openModalBtn: document.querySelector('[data-modal-open]'),
+    closeModalBtn: document.querySelector('[data-modal-close]'),
+    modal: document.querySelector('.backdrop'),
+    modalTitle: document.querySelector('.modal-title'),
+    inputUserName: document.querySelector('#username'),
+  };
+  
+  function toggleModal() {
+    refs.modal.classList.toggle('is-hidden');
+  }
+  
+  refs.form.addEventListener('submit', function (e) {
+    e.preventDefault();
+    if (refs.form.checkValidity()) {
+      refs.modalTitle.textContent = `${refs.inputUserName.value}, thank you!`;
+      toggleModal();
+      refs.form.reset();
+    }
+  });
+  
+  refs.closeModalBtn.addEventListener('click', function () {
+    toggleModal();
+  });
